6d642c4c5666845dee7f84e2b45f376bc50738cb
Prajith Ndz <prajithpalakkuda@gmail.com>
2017-02-21 14:26:11 +0530

    Removed unused modules and file Fixed terminal style issue

--- public/js/client.js
+++ public/js/client.js
@@ -10,25 +10,26 @@ client.run = function (options) {
             rows: options.screen.rows,
             cols: options.screen.cols,
             screenKeys: true,
-            cursorBlink: true
+            cursorBlink: true,
+            useStyle: true,
         });
 
         term.open(options.parent || document.body);
-	socket.onopen = function(e) {
+        socket.onopen = function(e) {
             socket.send(JSON.stringify({"screen":{"cols": options.screen.cols, "rows": options.screen.rows}}));
 
             term.on('data', function(data) {
                 socket.send(JSON.stringify({'stdin':data}));
             });
 
-	    socket.onmessage = function(event) {
-              json_msg = JSON.parse(event.data);
-	      term.write(json_msg.stdout);
+            socket.onmessage = function(event) {
+                json_msg = JSON.parse(event.data);
+                term.write(json_msg.stdout);
             };
 
-	    socket.onclose = function() {
-	        term.destroy();
-	    };
-       };
-       return {'socket': socket, 'term': term};
+            socket.onclose = function() {
+                term.destroy();
+            };
+        };
+        return {'socket': socket, 'term': term};
 };
